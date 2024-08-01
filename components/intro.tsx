8f2dd7f599f67ecf7c1eb6f6d5efd2f5e2f407b7
madirazz <razie.ahmadi93@gmail.com>
2024-08-01 14:59:34 -0700

    Add a profile image from an external source

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,5 +1,22 @@
 import React from "react";
+import Image from "next/image";
 
 export default function Intro() {
-  return <section>Intro</section>;
+  return (
+    <section>
+      <div className="flex items-center justify-center">
+        <div>
+          <Image
+            src="https://images.unsplash.com/photo-1515253564590-b7ec416b06be?q=80&w=3000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt="Razieh Portrait"
+            width="192"
+            height="192"
+            quality="95"
+            priority={true}
+            className="h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl"
+          />
+        </div>
+      </div>
+    </section>
+  );
 }
