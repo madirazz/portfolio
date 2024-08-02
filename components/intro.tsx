d2f05e70fcf5dac1c6d7835aac00b5c8e6fb2e13
madirazz <razie.ahmadi93@gmail.com>
2024-08-01 18:33:59 -0700

    Add waving hand emoji and change profile picture

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,22 +1,47 @@
+"use client";
+
 import React from "react";
 import Image from "next/image";
+import profileImage from "../public/profile.png";
+import { motion } from "framer-motion";
 
 export default function Intro() {
   return (
     <section>
       <div className="flex items-center justify-center">
-        <div>
-          <Image
-            src="https://images.unsplash.com/photo-1515253564590-b7ec416b06be?q=80&w=3000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            alt="Razieh Portrait"
-            width="192"
-            height="192"
-            quality="95"
-            priority={true}
-            className="h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl"
-          />
+        <div className="relative">
+          <motion.div
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 0.2, type: "tween" }}
+          >
+            <Image
+              src={profileImage}
+              alt="Razieh Image"
+              width="192"
+              height="192"
+              quality="95"
+              priority={true}
+              className="h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl"
+            />
+          </motion.div>
+
+          <motion.span
+            className="absolute bottom-0 right-0 text-3xl"
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              duration: 0.7,
+              type: "spring",
+              stiffness: 125,
+              delay: 0.1,
+            }}
+          >
+            👋🏻
+          </motion.span>
         </div>
       </div>
+      
     </section>
   );
 }
