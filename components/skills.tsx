edbdc9ccfd651416def2ea10049be4f215675c75
madirazz <razie.ahmadi93@gmail.com>
2024-08-11 03:08:02 -0700

    Enhance Skills component layout and styling

--- components/skills.tsx
+++ components/skills.tsx
@@ -4,11 +4,16 @@ import { skillsData } from "@/lib/data";
 
 export default function Skills() {
   return (
-    <section>
+    <section className="mb-28 max-w-[53rem] scroll-mt-28 text-center sm:mb-40">
       <SectionHeading>My Skills</SectionHeading>
-      <ul>
+      <ul className="flex flex-wrap justify-center gap-2 text-lg text-gray-800">
         {skillsData.map((skill, index) => (
-          <li key={index}>{skill}</li>
+          <li
+            className="bg-white border border-black/[0.1] rounded-xl px-5 py-3"
+            key={index}
+          >
+            {skill}
+          </li>
         ))}
       </ul>
     </section>
