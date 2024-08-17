9e4f714ba83021bb363f6deb1638b7aaeb176ef8
madirazz <razie.ahmadi93@gmail.com>
2024-08-17 01:07:37 -0700

    Created experience component

---
+++ components/experience.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import SectionHeading from './section-heading'
+
+export default function Experience() {
+  return <section className="experience">
+    <SectionHeading>My Experience</SectionHeading>
+  </section>
+}
