07bc5bb42d951bf621068d1115db0ab2fb297157
madirazz <razie.ahmadi93@gmail.com>
2024-08-24 02:58:32 -0700

    Enhance Contact section with form and styling improvements

--- components/contact.tsx
+++ components/contact.tsx
@@ -1,17 +1,29 @@
 import React from "react";
 import SectionHeading from "./section-heading";
+import { FaPaperPlane } from "react-icons/fa";
 
 export default function Contact() {
   return (
-    <section id="contact">
+    <section id="contact" className="mb-20 sm:mb-28 w-[min(100%,38rem)]">
       <SectionHeading>Contact me</SectionHeading>
-      <p>
+      <p className="text-gray-700">
         Please contact me directly at my{" "}
         <a className="underline" href="mailto:razie.ahmadi93@gmail.com">
           mail
         </a>{" "}
         or through this form
       </p>
+
+      <form className="mt-10 flex flex-col">
+        <input className="h-14 px-4 rounded-lg borderBlack" type="email" />
+        <textarea className="h-52 my-3 rounded-lg borderBlack p-4" />
+        <button
+          type="submit"
+          className="flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-gray-900 text-white rounded-full outline-none transition-all"
+        >
+          Submit <FaPaperPlane className="text-xs opacity-70 transition-all " />
+        </button>
+      </form>
     </section>
   );
 }
