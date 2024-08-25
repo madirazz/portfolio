239628420987afc39d1c77ba0a7066b4dee3ff01
madirazz <razie.ahmadi93@gmail.com>
2024-08-25 04:15:07 -0700

    Add validateString function to check if input is a string and within a specified maxLength

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -2,17 +2,30 @@
 
 import React from "react";
 import { Resend } from "resend";
+import { validateString } from "@/lib/utils";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendEmail = async (formData: FormData) => {
-  console.log(formData.get("senderEmail"));
-  console.log(formData.get("message"));
+  const senderEmail = formData.get("senderEmail");
+  const message = formData.get("message");
 
-  resend.emails.send({
+  if (!validateString(senderEmail, 500)) {
+    return {
+      error: "Invalid sender email",
+    };
+  }
+  if (!validateString(message, 5000)) {
+    return {
+      error: "Invalid message",
+    };
+  }
+
+  await resend.emails.send({
     from: "onboarding@resend.dev",
     to: "razie.ahmadi93@gmail.com",
     subject: "Message from contact form",
-    text: "hello from the other side",
+    reply_to: senderEmail as string,
+    text: message as string,
   });
 };
