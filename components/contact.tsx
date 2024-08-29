99341638dd4fc848d6e05057c4265dd97050ab5f
madirazz <razie.ahmadi93@gmail.com>
2024-08-28 17:36:15 -0700

    Created a button component for submit form

--- components/contact.tsx
+++ components/contact.tsx
@@ -6,6 +6,7 @@ import { FaPaperPlane } from "react-icons/fa";
 import { motion } from "framer-motion";
 import { useSectionInView } from "@/lib/hooks";
 import { sendEmail } from "@/actions/sendEmail";
+import SubmitButton from "./submit-button";
 
 export default function Contact() {
   const { ref } = useSectionInView("Contact");
@@ -58,13 +59,7 @@ export default function Contact() {
           maxLength={5000}
           placeholder="Your message"
         />
-        <button
-          type="submit"
-          className="flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-gray-900 text-white rounded-full outline-none transition-all  focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 "
-        >
-          Submit{" "}
-          <FaPaperPlane className="text-xs opacity-70 transition-all group-hover:translate-x-1 group-hover:-translate-y-1" />
-        </button>
+        <SubmitButton />
       </form>
     </motion.section>
   );
